23950f832cf0a093970140a2f9da3baacc9c2084
Shenghong <shenghong1123@outlook.com>
2017-06-29 23:57:53 -0700

    call google map API

---
+++ index.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const PORT_NUMBER = 8080;
+
+app.get('/',function (req, res) {
+  res.sendFile(path.join(__dirname + '/templates/index.html'));
+});
+
+app.listen(8080, function () {
+    console.log("Listening port: " + 8080);
+});
