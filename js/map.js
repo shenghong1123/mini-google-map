000091f84b094b561ca5d56e4c9f58cf5e106433
Shenghong <shenghong1123@outlook.com>
2017-06-30 23:09:19 -0700

    do the bearby search on front end

--- js/map.js
+++ js/map.js
@@ -1,11 +1,33 @@
+var DEFAULT_ZOOM = 15;
+var GOOGLE_API_KEY = 'AIzaSyBxcoRSUSKxeAQ6qV_2D5CovpUeC5UyToA';
+
 function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map($('#map')[0], {
-          zoom: 4,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
+    var position = {
+        lat: -25.363,
+        lng: 131.044
+    };
+    var map = new google.maps.Map($('#map')[0], {
+        zoom: DEFAULT_ZOOM,
+        center: position
+    });
+    var marker = new google.maps.Marker({
+        position: position,
+        map: map
+    });
+
+    $.ajax({
+        url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
+        data: {
+            'location': position.lat + ',' + position.lng,
+            'type': 'restaurant',
+            'key': GOOGLE_API_KEY,
+            'radius': 500
+        },
+        success: function(data) {
+            debugger;
+        },
+        failure: function(data) {
+            debugger;
+        }
+    });
+}
