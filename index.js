35a037779053fa7ed8f919ef76fee533c5606f0f
Shenghong <shenghong1123@outlook.com>
2017-07-05 00:11:54 -0700

    make code not depend on google api

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const path = require('path');
 const express = require('express');
+const request = require('request');
 const app = express();
 
 const PORT_NUMBER = 8080;
+const GOOGLE_KEY = 'AIzaSyBxcoRSUSKxeAQ6qV_2D5CovpUeC5UyToA';
+const NEARBY_SEARCH_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
 
 app.get('/',function (req, res) {
   res.sendFile(path.join(__dirname + '/templates/index.html'));
+});
+
+app.get('/nearby_search', function (req, api_res) {
+    var radius = req.query.radius ? req.query.redius : 150;
+    var params = {
+        'key': GOOGLE_KEY,
+        'location': req.query.location,
+        'radius': radius,
+        'type': req.query.type
+    };
+    request({url: NEARBY_SEARCH_URL, qs: params},
+        function(err, res, body) {
+            if (res.statusCode == 200) {
+                api_res.json(body);
+            } else {
+                // error handler
+            }
+        }
+    );
 });
 
 app.use('/css', express.static('css'))
--- js/map.js
+++ js/map.js
@@ -1,33 +1,37 @@
 var DEFAULT_ZOOM = 15;
 var GOOGLE_API_KEY = 'AIzaSyBxcoRSUSKxeAQ6qV_2D5CovpUeC5UyToA';
 
-function initMap() {
+$(function () {
+  function initMap() {
     var position = {
-        lat: -25.363,
-        lng: 131.044
+      lat: 32.878091,
+      lng: -117.227134
     };
     var map = new google.maps.Map($('#map')[0], {
-        zoom: DEFAULT_ZOOM,
-        center: position
+      zoom: DEFAULT_ZOOM,
+      center: position
     });
     var marker = new google.maps.Marker({
-        position: position,
-        map: map
+      position: position,
+      map: map
     });
 
     $.ajax({
-        url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
-        data: {
-            'location': position.lat + ',' + position.lng,
-            'type': 'restaurant',
-            'key': GOOGLE_API_KEY,
-            'radius': 500
-        },
-        success: function(data) {
-            debugger;
-        },
-        failure: function(data) {
-            debugger;
-        }
+      url: '/nearby_search',
+      data: {
+        'location': position.lat + ',' + position.lng,
+        'type': 'restaurant',
+        'key': GOOGLE_API_KEY,
+        'radius': 500
+      },
+      success: function (data) {
+        debugger;
+      },
+      failure: function (data) {
+        debugger;
+      }
     });
-}
+  }
+  
+  initMap();
+});
