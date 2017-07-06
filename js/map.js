a942f314212329a050eff29171e259fbdc5d1706
Shenghong Wang <shenghong@Shenghongs-MacBook-Pro.local>
2017-07-06 01:23:31 -0700

    add a toggle button to the detailed info view, have problem with img

--- js/map.js
+++ js/map.js
@@ -2,11 +2,11 @@ var DEFAULT_ZOOM = 15;
 var GOOGLE_API_KEY = 'AIzaSyBxcoRSUSKxeAQ6qV_2D5CovpUeC5UyToA';
 
 $(function () {
-  
+
   var DEFAULT_RADIUS = 1500;
-  var DEFAULT_LAT = 32.878091;
-  var DEFAULT_LNG = -117.227134;
-  
+  var DEFAULT_LAT = 37.773972;
+  var DEFAULT_LNG = -122.43129;
+
   function initMap() {
     var map = new google.maps.Map($('#map')[0], {
       zoom: DEFAULT_ZOOM,
@@ -15,19 +15,19 @@ $(function () {
         lng: DEFAULT_LNG
       }
     });
-    
+
     var params = {
       'location': new google.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
       'radius': DEFAULT_RADIUS,
       'type': 'restaurant'
     };
-    
+
     service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(params, function(results, status) {
+    service.nearbySearch(params, function (results, status) {
       if (status === google.maps.places.PlacesServiceStatus.OK) {
         var current_infowindow;
-        
-        _.each(results, function(place) {
+
+        _.each(results, function (place) {
           var marker = new google.maps.Marker({
             position: {
               'lat': place.geometry.location.lat(),
@@ -35,14 +35,14 @@ $(function () {
             },
             map: map
           });
-          
+
           var infowindow_content = '<div id="content">' + '<h1 id="firstHeading" class="firstHeading">' + place.name + '</h1>' + '</div>';
-          
+
           var infowindow = new google.maps.InfoWindow({
             content: infowindow_content
           });
-          
-          marker.addListener('click', function() {
+
+          marker.addListener('click', function () {
             if (current_infowindow) {
               current_infowindow.close();
             }
@@ -53,20 +53,28 @@ $(function () {
         });
       }
     });
+
+    $('.place-info-visibility-toggle').on('click', function () {
+      $('#place-info-wrapper').toggleClass('visible');
+      $('#place-info-wrapper .triangle-icon').toggleClass('left');
+    });
   }
-  
+
   function showDetailedInfo(place) {
     var params = {
       placeId: place['place_id']
     };
-    service.getDetails(params, function(place) {
-      $('#hero-header-wrapper img').attr('src', place.photos[0].getUrl({'maxWidth': 408, 'maxheight': 407}));
+    service.getDetails(params, function (place) {
+      $('#hero-header-wrapper img').attr('src', place.photos[0].getUrl({
+        'maxWidth': 408,
+        'maxheight': 407
+      }));
       $('.place-name').text(place['name']);
       $('.place-review-score').text(place['rating']);
       $('.place-type').text(place['types'][0]);
-      $('#place-info-wrapper').show();
+      $('#place-info-wrapper').addClass('visible');
     });
   }
-  
+
   initMap();
 });
